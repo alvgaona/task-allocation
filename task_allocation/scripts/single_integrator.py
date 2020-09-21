58b419547805284c91b86c91e5ed8b75fbc83587
Alvaro <alvgaona@gmail.com>
2020-09-21 11:36:40 -0300

    chores: Update a few scripts

--- task_allocation/scripts/single_integrator.py
+++ task_allocation/scripts/single_integrator.py
@@ -1,5 +1,11 @@
 #!/usr/local/bin/python3
 
+r""" Single-Integrator State Space Representation
+
+    .. math::
+        \dot{x} = u, x \in \mathbb{R}^{2}, u \in \mathbb{R}^2
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -8,11 +14,6 @@ from scipy.signal import StateSpace, lsim
 np.set_printoptions(suppress=True)
 
 if __name__ == '__main__':
-    r""" Single-Integrator State Space Representation
-    .. math::
-        \dot{x} = u, x \in \mathbb{R}^{2}, u \in \mathbb{R}^2
-    """
-
     A = np.zeros((2, 2))
     B = np.identity(2)
     C = np.identity(2)
@@ -25,13 +26,13 @@ if __name__ == '__main__':
     step_function = np.ones((T, 2))
 
     linear_function = np.hstack([
-        np.linspace(0, T - 1, T).reshape(-1, 1),
-        np.linspace(0, T - 1, T).reshape(-1, 1)
+        -0.59421658 * np.linspace(0, T - 1, T).reshape(-1, 1),
+        -0.59421658 * np.linspace(0, T - 1, T).reshape(-1, 1)
     ])
 
     t, y1, _ = lsim(sys, step_function, range(T))
     _, y2, _ = lsim(sys, linear_function, range(T))
-
+    
     plt.plot(t, y1[:, 0])
     plt.plot(t, y2[:, 0])
     plt.show()
